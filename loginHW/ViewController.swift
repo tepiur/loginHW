0ab6992f199d39ada53e9433748c0770d166da44
tepiur <st.iurin@mail.ru>
2023-11-24 01:16:23 +0500

    Initial Commit

---
+++ loginHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginHW
+//
+//  Created by Stefan Iurin on 24.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
